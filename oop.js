06097e36a5273c812fb2d936cdcd3f3b26fed909
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-12-06 16:49:42 -0800

    second concept --> Factories

--- oop.js
+++ oop.js
@@ -1,12 +1,13 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: () => {
-        console.log('draw');
-    }
-};
+
+function createCircle(radius){
+    return {
+        radius,
+        draw: () => {
+            console.log('draw');
+        }
+    };
+}
+
+const circle = createCircle(1);
 
 circle.draw();
